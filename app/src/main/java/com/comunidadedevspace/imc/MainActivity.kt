3274b932ed39e6833601a69ebc943e36f5b92d86
Felipe <fe.amazetti@gmail.com>
2024-08-27 18:34:56 -0300

    mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -39,6 +40,14 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                // navegar para a proxima tela
+                // criar layout para a proxima tela
+                // passar dados para a proxima tela
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
 
                 println("Resultado: $resultado")
             }
