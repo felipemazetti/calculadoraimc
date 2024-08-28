25188f7b93527452642ccd5a929fa88920afc000
Felipe <fe.amazetti@gmail.com>
2024-08-28 15:56:46 -0300

    Melhorando experiencia do usuário

--- app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -14,6 +15,7 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.primary_900)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -25,16 +27,23 @@ class ResultActivity : AppCompatActivity() {
         val tvResult = findViewById<TextView>(R.id.tv_result)
         val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
 
-        val classificacao: String =  if (result <= 18.5f){
-            "MAGREZA"
+        val classificacao: String
+
+        if (result <= 18.5f){
+            classificacao = "MAGREZA"
+            tvClassificacao.setTextColor(ContextCompat.getColor(/* context = */ this, /* id = */ R.color.magreza))
         }else if (result > 18.5f && result <= 24.9f){
-            "NORMAL"
+            classificacao = "NORMAL"
+            tvClassificacao.setTextColor(ContextCompat.getColor(/* context = */ this, /* id = */ R.color.normal))
         }else if (result > 25f && result <= 29.9f){
-            "SOBREPESO"
+           classificacao = "SOBREPESO"
+            tvClassificacao.setTextColor(ContextCompat.getColor(/* context = */ this, /* id = */ R.color.sobrepeso))
         }else if (result > 30f && result <= 39.9f ){
-            "OBESIDADE"
+           classificacao =  "OBESIDADE"
+            tvClassificacao.setTextColor(ContextCompat.getColor(/* context = */ this, /* id = */ R.color.obesidade))
         } else {
-            "OBESIDADE GRAVE"
+           classificacao =  "OBESIDADE GRAVE"
+            tvClassificacao.setTextColor(ContextCompat.getColor(/* context = */ this, /* id = */ R.color.obesidade_grave))
         }
 
         tvClassificacao.text = classificacao
